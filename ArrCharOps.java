81cdb226c7e105ea841c777947ebcf11ee9ec4fa
noamwugman <noamwug24@gmail.com>
2025-11-24 12:58:07 +0200

    noam

--- ArrCharOps.java
+++ ArrCharOps.java
@@ -151,8 +151,6 @@ public class ArrCharOps {
      *         return -2 if there is an error with the input.
      */
     public static int compareTo(String str1, String str2) {
-        str1 = str1.toLowerCase();
-        str2 = str2.toLowerCase();
         if (str1 == "" || str2 == "")
             return -2;
         for (int i = 0; i < Math.min(str1.length(), str2.length()); i++) {
